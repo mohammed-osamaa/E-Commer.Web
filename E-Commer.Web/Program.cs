41ada4d73c20783e08fb33b4c4ca05815ae1fcaa
Mohamed Osamaa <mohammedosama1a2@gmail.com>
2025-04-13 16:35:30 +0200

    Add project files.

---
+++ E-Commer.Web/Program.cs
@@ -0,0 +1,34 @@
+
+namespace E_Commer.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(); 
+
+            #endregion
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
